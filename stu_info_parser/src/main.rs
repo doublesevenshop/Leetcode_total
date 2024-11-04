829ac7a184eb592f81902d9a8ef15549b87d19b1
xyw <1416642324@qq.com>
2024-11-04 10:27:07 +0800

    先做一个简单的终端输出的东东

--- stu_info_parser/src/main.rs
+++ stu_info_parser/src/main.rs
@@ -1,4 +1,16 @@
-use std::collections::HashMap;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    // 先把空的字符串放到这里
+    let mut input = String::new();
+    
+    loop {
+        print!("> ");
+        // 确保它能够直接刷新
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+    }
 }

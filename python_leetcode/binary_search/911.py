3e002452b303ebb65b8523a0462654a70973b21a
xyw <1416642324@qq.com>
2025-02-02 21:35:54 +0800

    思考二分查找三种模式的优缺点

--- python_leetcode/binary_search/911.py
+++ python_leetcode/binary_search/911.py
@@ -19,7 +19,7 @@ class TopVotedCandidate:
         l, r = 0, len(self.times) - 1
         # 找到满足 times[l] <= t 的最大的 l
         # 也可以使用内置的二分查找的包来确定 l
-        l = bisect.bisect(self.times, t) - 1
+        l = bisect(self.times, t) - 1
         return self.tops[l]
 
 # Your TopVotedCandidate object will be instantiated and called as such:
--- python_leetcode/binary_search/ttest.py
+++ python_leetcode/binary_search/ttest.py
@@ -1,6 +1,6 @@
 from typing import List
 import collections
 import pytest
-from bisect import bisect_left, bisect_right
+from bisect import bisect_left, bisect_right, bisect
 from collections import defaultdict
 import math
